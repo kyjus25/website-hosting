9e8702940cbde1c7a417b1d2bbaca838bb8e3527
Dennis <bigdww@hotmail.com>
2021-12-04 20:53:34 -0600

    add Led

---
+++ led/src/partials/Sidebar.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<aside>
+    <ul>
+        <li><Link to="/pages/about"><i class="fas fa-address-card"/> About</Link></li>
+        <li><Link to="/pages/dontate"><i class="fas fa-hand-holding-usd"/> Donate</Link></li>
+        <li><Link to="/pages/contact"><i class="fas fa-envelope"/>Contact</Link></li>
+        <li><Link to="/pages/mission"><i class="fas fa-flag-checkered"/> Our Mission</Link></li>
+    </ul>
+</aside>
