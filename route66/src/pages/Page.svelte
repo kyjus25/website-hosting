6e9134594a8ff60e1a3ee6820a2088698997cb7e
Dennis <bigdww@hotmail.com>
2021-12-04 20:59:44 -0600

    add rout 66

---
+++ route66/src/pages/Page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SvelteMarkdown from 'svelte-markdown';
+    export let id = null;
+	export let pages = [];
+    $: content = pages.find(i => i.title.toLowerCase().replace(new RegExp(" ", "g"), '-') === id)?.content;
+</script>
+<article>
+    {#if content}
+        <SvelteMarkdown source={content} />
+    {:else}
+        <h1>404</h1>
+    {/if}
+</article>
