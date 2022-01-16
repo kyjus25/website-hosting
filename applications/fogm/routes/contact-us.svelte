ec622fa2fedb1330404853a5043ffe3f3b410d70
Justin White <kyjus25@gmail.com>
2022-01-16 11:20:13 -0600

    Create pages

---
+++ applications/fogm/routes/contact-us.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Gradient from "../components/Gradient.svelte";
+</script>
+
+<Gradient />
+
+<article>
+    <div class="wrapper">
+        <h1>Contact Us</h1>
+        <p>
+            Send us an email at <a href="mailto:FireOfGodMinistries3@gmail.com"
+                >FireOfGodMinistries3@gmail.com</a
+            >
+        </p>
+    </div>
+</article>
---
+++ applications/fogm/routes/order.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Gradient from "../components/Gradient.svelte";
+    // window.open("https://webtv.international/live", "_blank");
+</script>
+
+<Gradient />
+
+<article>
+    <div class="wrapper">
+        <h1>Order</h1>
+        <p>We are currently restocking our inventory. Check back soon!</p>
+    </div>
+</article>
