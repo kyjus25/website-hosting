1ac534324a2165b22f81bda112937cb3f0a0a02f
Dennis <bigdww@hotmail.com>
2022-02-05 20:44:06 -0600

    caresell pic updates

--- applications/fogm/routes/usa-project-charleston-il.svelte
+++ applications/fogm/routes/usa-project-charleston-il.svelte
@@ -5,28 +5,28 @@
 
 <div class="w-full carousel mb-3 opacity-80">
   <div id="slide1" class="relative w-full carousel-item">
-    <img src="https://picsum.photos/id/500/800/300" class="w-full" alt="led logo"> 
+    <img src="/fogm/usa-project.JPG" class="w-full" alt="usa pic"> 
     <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" class="btn btn-circle">❮</a> 
       <a href="#slide2" class="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" class="relative w-full carousel-item">
-    <img src="https://picsum.photos/id/501/800/300" class="w-full"alt="led logo"> 
+    <img src="/fogm/Full Layout Of Proposed Land Area.png" class="w-full"alt="land"> 
     <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" class="btn btn-circle">❮</a> 
       <a href="#slide3" class="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" class="relative w-full carousel-item">
-    <img src="https://picsum.photos/id/502/800/300" class="w-full"alt="led logo"> 
+    <img src="/fogm/tent.jpeg" class="w-full"alt="tent"> 
     <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" class="btn btn-circle">❮</a> 
       <a href="#slide4" class="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" class="relative w-full carousel-item">
-    <img src="https://picsum.photos/id/503/800/300" class="w-full"alt="led logo"> 
+    <img src="/fogm/camp.jpeg" class="w-full"alt="camp ground"> 
     <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" class="btn btn-circle">❮</a> 
       <a href="#slide1" class="btn btn-circle">❯</a>
